bfd190e98610d19af96626bf702effe61371631e
jakobvaa <jakob.vaardal@gmail.com>
2018-09-19 13:50:32 +0200

    Eksempel bruk av media og kategori komponent i kommentar

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import Category from './Category';
+import Media from './Media';
+
 
 class App extends Component {
   render() {
@@ -13,9 +16,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
       </div>
     );
   }
 }
 
 export default App;
+//Eksempel bruk av category og Media
+/*
+<Category text = "Katt"/>
+<br></br>
+<Media name = "picture" categories = {[{id:0, name:"Dyr"},{id:1 , name:"Biler"},{id:2, name:"Peperonice"}]}/>
+*/
