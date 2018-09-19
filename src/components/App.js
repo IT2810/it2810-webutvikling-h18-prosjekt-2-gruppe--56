a3154372ff3dd93ae5bb2778cc63edb30888e4bf
jakobvaa <jakob.vaardal@gmail.com>
2018-09-19 18:53:31 +0200

    Endringer for å ha fungerende category component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
-import Category from './Category';
-import Media from './Media';
+
+import {createDataList} from '../util/Util.js';
+import CategoryPane from './CategoryPane.js';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div id="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -16,11 +17,27 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-
+        <CategoryPane/>
       </div>
     );
   }
 }
+//
+
+/*
+If good website with dynamic content
+const categoryPicture = ["Dyr","Mat","Kjøretøy"];
+const categoryText = ["Ibsen","Lotepus","Modleifen"];
+const categoryData = [createDataList(categoryPicture, "Pictures" , 0),
+createDataList(categoryText, "Text", 1)];
+console.log(categoryData);
+*/
+/*format:    [{id : 0,
+name : "Pictures",
+array: [{id : 0, text : "Dyr", ....}]}]
+*/
+//Should make static counter or pass an argument as id, but easylife
+
 
 export default App;
 //Eksempel bruk av category og Media
--- src/components/Media.js
+++ src/components/Media.js
@@ -7,15 +7,17 @@ export default function Media(props){
   const array = props.categories;
   console.log(array);
   return(
-    <form action = "">
-      {array
-      .map(category =>
-        //Must implement handleCategoryClick in App, send from categoryPane
-        //handleClick = props.handleCategoryClick as prop in category
-        
-        <Category id = {category.id}  text = {category.name} name = {props.name}/>
-      )}
-    </form>
+    <div className = "media-flex-item">
+      <h3 className = "media-h3">{props.name}</h3>
+      <form className = "media-form" action = "">
+        {array
+        .map(category =>
+          //Must implement handleCategoryClick in App, send from categoryPane
+          //handleClick = props.handleCategoryClick as prop in category
+          <Category id = {category.id}  text = {category.name} name = {props.name}/>
+        )}
+      </form>
+    </div>
   );
 }
 //const array = props.categories;
