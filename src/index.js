eb026bcf885d4f8a227905f9b3d3ac1068099f4e
Scott Gullaksen <scotty_boy_96@hotmail.com>
2018-09-20 13:19:12 +0200

    #16-tilrettelegger merge

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import Art from './components/Art';
 
-ReactDOM.render(<Art />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
