5b2c1778626a2f28260a32b5a833558073b721a2
Scott Gullaksen <scotty_boy_96@hotmail.com>
2018-09-20 14:45:13 +0200

    #10-Utkast til Artpane ferdig

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import Artpane from './components/Artpane.js';
 
-ReactDOM.render(<Artpane />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
