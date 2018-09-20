d113f78f6dcb8bb7ec6450ed8772086e29810212
Scott Gullaksen <scotty_boy_96@hotmail.com>
2018-09-20 14:03:59 +0200

    Artpanekomponent satt opp

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import Artpane from './components/Artpane.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Artpane />, document.getElementById('root'));
 registerServiceWorker();
