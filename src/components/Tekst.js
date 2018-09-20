4d09c47072b1b80ad3ce70afd94995d5eb8f0346
Scott Gullaksen <scotty_boy_96@hotmail.com>
2018-09-20 13:17:27 +0200

    #16-lagd ferdig utkast av artkomponent

--- src/components/Tekst.js
+++ src/components/Tekst.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
 import './Tekst.css';
-<<<<<<< HEAD
-=======
-import ReactSVG from 'react-svg-inline'
->>>>>>> develop
 
 class Tekst extends Component {
     render() {
     	return (
-<<<<<<< HEAD
 			<div className = "Tekstboks">
 				<p>{this.props.tekst}</p>
-=======
-			<div className = "Tekst">
-				{this.props.tekst}
->>>>>>> develop
 			</div>
     	);
     }
