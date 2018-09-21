58144fed0655331875a27cfd8fb14808b8eee12d
jakobvaa <jakob.vaardal@gmail.com>
2018-09-21 13:26:37 +0200

    Fiksing av bugs, nrmer seg

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,7 @@ class App extends Component {
             activeImageCat: 0,
             activeSoundCat: 3,
             activeTextCat: 6,
+            data:{}
         }
         this.handleTabClick = this.handleTabClick.bind(this);
         this.handleCategoryClick = this.handleCategoryClick.bind(this);
@@ -43,29 +44,30 @@ class App extends Component {
     }
 
     handleCategoryClick = (id) => {
-        if (id in [0,1,2]){
-            newActives = [id, this.state.activeSoundCat, this.state.activeTextCat]
-        } else if (val in [3,4,5]) {
-            newActives = [this.state.activeImageCat, id, this.state.activeTextCat]
-        } else{
-            newActives = [this.state.activeImageCat,  this.state.activeSoundCat, id]
-        }
-        let dataIndex = this.state.activeTab.toString() + id.toString();
-        let updatedData = getData(dataIndex);
-        let new_data = this.state.data
-        new_data[dataIndex] = updatedData;
-        this.setState({
-            activeTextCat: newActives[0],
-            activeSoundCat: newActives[1],
-            activeTextCat: newActives[2],
-            data: new_data
-        })
+      let newActives = [];
+      if (id in [0,1,2]){
+          newActives = [id, this.state.activeSoundCat, this.state.activeTextCat]
+      } else if (id in [3,4,5]) {
+          newActives = [this.state.activeImageCat, id, this.state.activeTextCat]
+      } else{
+          newActives = [this.state.activeImageCat,  this.state.activeSoundCat, id]
+      }
+      let dataIndex = this.state.activeTab.toString() + id.toString();
+      let updatedData = this.getData(dataIndex);
+      let new_data = this.state.data
+      new_data[dataIndex] = updatedData;
+      this.setState({
+          activeTextCat: newActives[0],
+          activeSoundCat: newActives[1],
+          activeTextCat: newActives[2],
+          data: new_data
+      })
     }
 
     getData(dataIndex){
         if (!dataIndex in this.state.data){
-            let media = determineMedia(val2)
-             return fetchFile(media[0], dataIndex.charAt(0), dataIndex.charAt(1), media[1])
+            let media = this.determineMedia(dataIndex.charAt(0).parseInt());
+            return this.fetchFile(media[0], dataIndex.charAt(0), dataIndex.charAt(1), media[1])
         }
         else {
             return(this.state.data[dataIndex])
@@ -100,8 +102,8 @@ class App extends Component {
                 <Tabsbar handleTabClick={this.handleTabClick} activeTab ={this.state.activeTab} />
             </div>
             <div className = "bodyPane">
-                <Art Image = {this.state.data[this.activeTab.toString()+this.activeImageCat.toString()]}
-                    Text = {this.state.data[this.activeTab.toString()+this.activeTextCat.toString()]} />
+                <Art image = {this.state.data[this.state.activeTab.toString()+this.state.activeImageCat.toString()]}
+                    Text = {this.state.data[this.state.activeTab.toString()+this.state.activeTextCat.toString()]} />
                 <CategoryPane method ={this.handleCategoryClick}/>
             </div>
         </div>
